c0746669e9b822446ed84fc4565474fb8c88c549
ALFA <alfa.sunny.22@gmail.com>
2022-10-15 06:32:43 +0600

    First Commit With Question List Link

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Question List Link:  https://drive.google.com/file/d/1FMdN_OCfOI0iAeDlqswCiC2DZzD4nPsb/view
+    }
+}
